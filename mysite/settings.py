fc1699deaa353cf5223187fdc2975a5823b85606
Maeshima <mesm0826@gmail.com>
2018-01-14 22:20:23 +0900

    My Django Girls app

--- mysite/settings.py
+++ mysite/settings.py
@@ -123,7 +123,7 @@ STATIC_URL = '/static/'
 import dj_database_url
 DATABASES['default'] = dj_database_url.config()
 
-SECURE_PROXY_SSL_HEADER = ('HTTP_XFORWARD_PROTO', 'https')
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARD_PROTO', 'https')
 
 ALLOWED_HOSTS = ['*']
 
