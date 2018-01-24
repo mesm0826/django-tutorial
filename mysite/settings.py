45a6616ba36eb6d6f09722f869416925cb071540
Maeshima <mesm0826@gmail.com>
2018-01-24 22:32:11 +0900

    Added more views to the website.

--- mysite/settings.py
+++ mysite/settings.py
@@ -123,9 +123,9 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
 STATIC_URL = '/static/'
-#追記
+
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, "static"),
 )
 
 """
